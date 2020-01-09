19d8b8e6ac3f3bf74d5bd234e1c00a9a8656f658
Jason_Dev <nosaj_1793@hotmail.com>
2020-01-09 10:22:37 -0600

    First commit
    Creación del proyecto

---
+++ PrediCiclo/PrediCiclo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrediCiclo
+//
+//  Created by Jason Sa on 1/9/20.
+//  Copyright © 2020 Zublime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
