8b4b978d09adf4af80d11781cd2167dd896cb7cb
DanielBarcenas97 <barcenas.proteco@gmail.com>
2020-01-20 02:03:00 -0600

    Navegación

---
+++ PrediCiclo/PrediCiclo/Controller/Registro2Periodo.swift
@@ -0,0 +1,30 @@
+//
+//  Registro2Periodo.swift
+//  PrediCiclo
+//
+//  Created by Daniel Barcenas on 1/20/20.
+//  Copyright © 2020 Zublime. All rights reserved.
+//
+
+import UIKit
+
+class Registro2Periodo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
